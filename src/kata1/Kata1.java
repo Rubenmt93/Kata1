b7729415b9b63726902aa7e903dce3bfe6d75ffc
Usuario <Usuario@PC-0857>
2017-10-10 16:39:48 +0100

    Segunda versión de la Kata1

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,13 +1,19 @@
 
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     
     public static void main(String[] args) {
-        Person person= new Person("Rubén",new Date(93,4,3));
+        Calendar date;
+        date=GregorianCalendar.getInstance();
+        date.set(1993,5,3);
+        
+        Person person= new Person("Rubén",date);
         System.out.println(person.getName()+" tiene "+ person.getAge()+ " años");
         
     }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -5,13 +5,17 @@
  */
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class Person {
     private final String name;
-    private final Date birthday;
-    public Person(String name, Date birthday){
+    private final Calendar  birthday;
+    private final long MILLISECONDS_PER_YEAR= (long)(1000*60*60*24*365.25);
+    
+    public Person(String name, Calendar birthday){
         this.name=name;
         this.birthday=birthday;
     }
@@ -19,12 +23,19 @@ public class Person {
     public String getName(){
         return name;
     }
-    public Date getBirthday(){
+    public Calendar getBirthday(){
         return birthday;
     }
     
     public int getAge(){
-        return (int) ((new Date().getTime()- birthday.getTime())/31536000000L);
+        Calendar today=GregorianCalendar.getInstance();
+        return(int)(milliSecondsToYear(today.getTimeInMillis()-
+                birthday.getTimeInMillis()));
+       
+    }
+    
+    public long milliSecondsToYear(long milles){
+        return milles/MILLISECONDS_PER_YEAR;
     }
 
 
